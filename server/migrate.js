e348eacecde16a0859d4d7756ca12854e71b3a1f
Nazar <nazar@sakharenko.com>
2018-03-31 13:43:42 +0300

    data

--- server/migrate.js
+++ server/migrate.js
@@ -4,6 +4,10 @@ const tj = require('@mapbox/togeojson');
 const fs = require('fs');
     // node doesn't have xml parsing or a dom. use xmldom
 const DOMParser = require('xmldom').DOMParser;
+require('./databases/mongo.db');
+const pointsModel = require('./models/geoJSON.model');
+
+
 
 const kml = new DOMParser().parseFromString(fs.readFileSync('map.kml', 'utf8'));
 
@@ -11,3 +15,14 @@ const converted = tj.kml(kml);
 
 const convertedWithStyles = tj.kml(kml, { styles: true });
 
+const p = [];
+
+convertedWithStyles.features.forEach((item) => {
+    p.push((new pointsModel({feature: item})).save());
+});
+
+Promise.all(p).then(res => {
+    console.log(res);
+}).catch(e => {
+    console.log(e);
+});
