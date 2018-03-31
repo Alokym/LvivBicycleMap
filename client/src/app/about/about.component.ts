aa651088d45207dfe48bea508d7879b0aa98ab9a
ibakun <ibakun@noltic.com>
2018-03-31 18:06:31 +0300

    add placeholder for about

--- client/src/app/about/about.component.ts
+++ client/src/app/about/about.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  message = {
+    from: 'yo',
+    subject: 'test',
+    content: 'content'
+  };
   constructor() { }
 
   ngOnInit() {
