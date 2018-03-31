6b430310981fdaf5f1fc0cf062b1074c0d03ad50
Petro Pokydko <petro.pokydko@gmail.com>
2018-03-31 16:18:41 +0300

    error fixed

--- client/src/app/route-definitions.ts
+++ client/src/app/route-definitions.ts
@@ -1,12 +1,10 @@
-import {EventsComponent} from "./events/events.component";
-import {NewsFeedComponent} from "./news-feed/news-feed.component";
-import {AboutComponent} from "./about/about.component";
-import {FeedbackComponent} from "./feedback/feedback.component";
-import {Routes} from "@angular/router";
-import {MapComponent} from "./map/map.component";
+import {EventsComponent} from './events/events.component';
+import {NewsFeedComponent} from './news-feed/news-feed.component';
+import {AboutComponent} from './about/about.component';
+import {FeedbackComponent} from './feedback/feedback.component';
+import {Routes} from '@angular/router';
 
 export const RouteDefinitions: Routes = [
-  { path: '', component: MapComponent },
   { path: 'about', component: AboutComponent },
   { path: 'events', component: EventsComponent },
   { path: 'news-feed', component: NewsFeedComponent },
