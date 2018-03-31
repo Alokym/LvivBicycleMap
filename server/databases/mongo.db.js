f62d12747860141051b8a358fd040c061ef1d7e7
Nazar <nazar@sakharenko.com>
2018-03-31 13:18:49 +0300

    express with points

---
+++ server/databases/mongo.db.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const env = process.env.NODE_ENV || 'local';
+
+const config = require(`../config/${env}.json`);
+const mongoose = require('mongoose');
+
+mongoose.connect(config.mongo);
+
+mongoose.connection.on('error', (error) => {
+    console.error('connection error:', error);
+    process.exit(-1);
+});
+mongoose.connection.once('open', () => {
+    console.info('mongo connected');
+});
