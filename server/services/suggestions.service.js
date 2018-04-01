d02ecbe9fefffeba4a6e8f717836b66e5c2df7f0
Nazar <nazar@sakharenko.com>
2018-04-01 13:02:30 +0300

    update date

--- server/services/suggestions.service.js
+++ server/services/suggestions.service.js
@@ -42,6 +42,7 @@ async function approve(id) {
             }
 
             suggestion.status = 'approved';
+            suggestion.updated = Date.now();
 
             suggestion
                 .save()
@@ -65,6 +66,7 @@ async function reject(id) {
             }
 
             suggestion.status = 'rejected';
+            suggestion.updated = Date.now();
 
             suggestion
                 .save()
